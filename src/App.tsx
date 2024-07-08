1edb7f983c3e88a18c4692a5920f576d9e12e147
sepidsoroush <s.soroush2012@gmail.com>
2024-07-08 21:53:51 +0300

    setup supabase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import "./App.css";
+import supabase from "./config/supabaseConfig";
+import { useEffect, useState } from "react";
+
+export interface Food {
+  id: string;
+  name: string;
+  ingredients?: string[];
+  lastCooked?: Date;
+  type?: string;
+}
 
 function App() {
-  return <div>Home page</div>;
+  const [foods, setFoods] = useState<Food[] | null>([]);
+  const [fetchErrors, setFetchErrors] = useState<string | null>(null);
+
+  useEffect(() => {
+    getFoods();
+  }, []);
+
+  async function getFoods() {
+    const { data, error } = await supabase.from("foods").select();
+    if (error) {
+      setFetchErrors("Could not fetch the foods list");
+      setFoods(null);
+      console.log(error);
+    }
+    if (data) {
+      setFoods(data);
+      setFetchErrors(null);
+    }
+  }
+
+  return (
+    <>
+      {fetchErrors && <p>{fetchErrors}</p>}
+      {foods && (
+        <ul>
+          {foods.map((food) => (
+            <li key={food.id}>{food.name}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
 
 export default App;
