a62478e1c90a55520b696f468c0735a1c5aa3572
sepidsoroush <s.soroush2012@gmail.com>
2024-07-08 22:07:01 +0300

    add root layout and routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,17 @@
-import supabase from "./config/supabaseConfig";
-import { useEffect, useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-export interface Food {
-  id: string;
-  name: string;
-  ingredients?: string[];
-  lastCooked?: Date;
-  type?: string;
-}
+import RootLayout from "./pages/Root";
+import MealsPage from "./pages/Meals";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [{ index: true, element: <MealsPage /> }],
+  },
+]);
 
 function App() {
-  const [foods, setFoods] = useState<Food[] | null>([]);
-  const [fetchErrors, setFetchErrors] = useState<string | null>(null);
-
-  useEffect(() => {
-    getFoods();
-  }, []);
-
-  async function getFoods() {
-    const { data, error } = await supabase.from("foods").select();
-    if (error) {
-      setFetchErrors("Could not fetch the foods list");
-      setFoods(null);
-      console.log(error);
-    }
-    if (data) {
-      setFoods(data);
-      setFetchErrors(null);
-    }
-  }
-
-  return (
-    <>
-      {fetchErrors && <p>{fetchErrors}</p>}
-      {foods && (
-        <ul>
-          {foods.map((food) => (
-            <li key={food.id}>{food.name}</li>
-          ))}
-        </ul>
-      )}
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
