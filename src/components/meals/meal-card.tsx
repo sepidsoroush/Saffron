d66847a1922e50de18f4f4421ee3243614dad10e
sepidsoroush <s.soroush2012@gmail.com>
2024-07-24 23:20:26 +0300

    fix the meals list grid layout

--- src/components/meals/meal-card.tsx
+++ src/components/meals/meal-card.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 import { useAppSelector } from "@/store/hooks";
 import { selectCompositionsByMealId } from "@/store/compositions/compositions.selector";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { ScrollArea } from "../ui/scroll-area";
 import IngredientListItem from "@/components/ingredients/ingredient-list-item";
+
 import { Meal } from "@/types";
 
 type Props = {
@@ -17,14 +19,19 @@ export const MealCard = ({ meal }: Props) => {
 
   return (
     <Link to={`/meals/${meal.id}`}>
-      <Card className="my-2">
+      <Card className="my-2 overflow-x-hidden">
         <CardTitle className="text-lg px-4 py-2">{meal.name}</CardTitle>
         <CardContent className="px-4 py-0">
-          <ul className="pb-2">
-            {ingredientsInRecipe.map((comp) => (
-              <IngredientListItem key={comp.id} ingredient={comp.ingredient} />
-            ))}
-          </ul>
+          <ScrollArea className="md:h-56 pb-4">
+            <ul className="pb-2">
+              {ingredientsInRecipe.map((comp) => (
+                <IngredientListItem
+                  key={comp.id}
+                  ingredient={comp.ingredient}
+                />
+              ))}
+            </ul>
+          </ScrollArea>
         </CardContent>
       </Card>
     </Link>
