dbf06be03d860bce75fb53f6b2a6af90c051e8e6
sepidsoroush <s.soroush2012@gmail.com>
2024-08-01 17:04:37 +0300

    update error page style

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
 export default function ErrorPage() {
+  const navigate = useNavigate();
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+    <section className="flex flex-col justify-center items-center">
+      <img src="error.svg" />
+      <h1 className="text-xl font-semibold tracking-tighter">
         404 - Page Not Found
       </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
+      <p className="my-4">The page you are looking for does not exist.</p>
+      <Button
+        variant="link"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Go back to home page
+      </Button>
     </section>
   );
 }
