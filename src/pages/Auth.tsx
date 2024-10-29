79ba5503bd949d7693c2ab773683cf3a421aba75
sepidsoroush <s.soroush2012@gmail.com>
2024-10-29 14:54:50 +0200

    remove deprecated supabase Auth UI dependency

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,45 +1,23 @@
-import { useEffect } from "react";
-import { useAuth } from "@/hooks/useAuth";
-import { useNavigate } from "react-router-dom";
-
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useState } from "react";
 import supabase from "@/config/supabaseConfig";
 
 const AuthPage = () => {
-  const { user } = useAuth();
-  const navigate = useNavigate();
+  const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    if (user) {
-      navigate("/schedule");
-    }
-  }, [user, navigate]);
+  // Social Sign-in
+  const handleOAuthSignIn = async (provider: "google" | "facebook") => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider,
+      options: { redirectTo: window.location.origin },
+    });
+    if (error) setError(error.message);
+  };
 
   return (
-    <div className="relative grid h-[calc(100vh-3rem)] grid-cols-1 overflow-hidden">
-      <div className="relative">
-        <div
-          className="absolute inset-0 bg-cover"
-          style={{
-            backgroundImage:
-              "url(https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80)",
-          }}
-        />
-        <div className="absolute inset-0 bg-gradient-to-t from-background to-background/60 md:to-background/40" />
-
-        <div className="container absolute top-1/2 col-span-1 flex -translate-y-1/2 items-center md:static md:col-span-2 md:flex md:translate-y-0 lg:col-span-1 justify-center my-10">
-          <div className="w-4/5 md:w-1/2 lg:w-1/3">
-            <Auth
-              supabaseClient={supabase}
-              appearance={{ theme: ThemeSupa }}
-              providers={["google", "apple", "facebook"]}
-              onlyThirdPartyProviders={true}
-              redirectTo={`${window.location.origin}`}
-            />
-          </div>
-        </div>
-      </div>
+    <div className="auth-container">
+      <button onClick={() => handleOAuthSignIn("google")}>Google</button>
+      <button onClick={() => handleOAuthSignIn("facebook")}>Facebook</button>
+      {error && <p className="error">{error}</p>}
     </div>
   );
 };
